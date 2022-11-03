ae97c4b69bfdd9818d95d167b5972f6945179c6b
Albert <zzzdmw@hotmail.com>
2022-11-03 14:24:05 +0800

    feat: 新增test一些测试样式

--- pages/test/test.js
+++ pages/test/test.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    popupShow: false,
   },
 
   /**
@@ -62,5 +62,15 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+
+  popupClick() {
+    this.setData({
+      popupShow: !this.data.popupShow
+    })
+  },
+
+  popupContentClick() {
+    console.log('popupContentClick');
   }
 })
