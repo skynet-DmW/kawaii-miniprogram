45be11620edbbb905f3ff7a63422ff976f4a5e60
Albert <zzzdmw@hotmail.com>
2022-11-25 16:41:00 +0800

    upda: 骰子

--- pages/dice/dice.js
+++ pages/dice/dice.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    flag: false
   },
 
   /**
@@ -62,5 +62,11 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+
+  start() {
+    this.setData({
+      flag: !this.data.flag
+    })
   }
 })
